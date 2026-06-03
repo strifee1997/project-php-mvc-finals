a11d4f723fa512e47eef73c7772446cd4d634e2a
Neo Kent <neokentdurano846@gmail.com>
2026-06-03 11:37:36 +0800

    Refactor Findable interface to use mixed type
    Updated return type of find method to mixed.

--- core/Database/Findable.php
+++ core/Database/Findable.php
@@ -1,11 +1,11 @@
 <?php
-
+//read, Interface Segregation Principle
 declare(strict_types=1);
 
 namespace Core\Database;
 
 interface Findable
 {
-    public function find(int $id): array|false;
+    public function find(int $id): mixed;
     public function all(): array;
 }
